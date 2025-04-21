25297ce148d64d4e4f8e50b0df28869937079fc1
Seongeun Baek <100seongeun@gmail.com>
2025-04-21 14:31:51 +0900

    login1
    login1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+
+    internal class Program
+    {
+
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hellow, World!! this is login");
+        }
+    }
+}
